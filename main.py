d27efece898d7e8c389f7ea715156864fa515029
sizzlyo <sizzlyo@yahoo.com>
2025-12-13 13:01:27 -0500

    experimenting with which pins do which lights

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import RPi.GPIO as GPIO
 bluePin = 11
 greenPin = 15
 redPin = 13
+fiveVPin = 17
 
 
 
@@ -28,6 +29,7 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(redPin, GPIO.OUT)
 GPIO.setup(greenPin, GPIO.OUT)
 GPIO.setup(bluePin, GPIO.OUT)
+GPIO.setup(fiveVPin, GPIO.OUT)
 
 
 
@@ -37,13 +39,14 @@ def all_off():
     GPIO.output(redPin, GPIO.LOW)
     GPIO.output(greenPin, GPIO.LOW)
     GPIO.output(bluePin, GPIO.LOW)
+    GPIO.setup(fiveVPin, GPIO.LOW)
 
 
 
 
 
 try:
-
+    all_off()
 
     print("Testing RED...")
     GPIO.output(redPin, GPIO.HIGH)
